4c91cc27be9d28d30f52c9eecced70eded07935d
majid <alinejad.mj@gmail.com>
2023-08-07 10:58:15 +0330

    delete 1 file and update 1 file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { Consoler } from "../dist/consoler";
 
 export const pp = new Consoler({
   nodeEnv: "majid",
-  password: "74876f3a13ecf1977dabb3cee7f6a9cfc05d414b5702eaa308bb9d4ba62e136c",
+  password: "9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0",
   defaultDeveloperMode: ["SUCCESS"],
   onMessageCallback: () => {},
 });
 
 pp.log("consoler");
 //@ts-ignore
-window.verbose = pp.verbose;
+window.verbose = pp;
