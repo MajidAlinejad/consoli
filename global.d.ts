ecf9fa928a123a2354c1fda26c7eb2e3a3147b09
majid <alinejad.mj@gmail.com>
2023-08-07 10:44:02 +0330

    delete 1 file and update 2 files

--- global.d.ts
+++
@@ -1,3 +0,0 @@
-interface Window {
-  verbose: () => Promise<void>;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
 import { Consoler } from "../dist/consoler";
-// const c = new Consoler({
-//   nodeEnv: "majid",
-//   password: "74876f3a13ecf1977dabb3cee7f6a9cfc05d414b5702eaa308bb9d4ba62e136c",
-//   defaultDeveloperMode: ["SUCCESS"],
-//   onMessageCallback: () => {},
-// });
-
-// c.log("hi");
 
 export const pp = new Consoler({
   nodeEnv: "majid",
@@ -15,6 +7,6 @@ export const pp = new Consoler({
   onMessageCallback: () => {},
 });
 
-pp.log("majid");
-
+pp.log("consoler");
+//@ts-ignore
 window.verbose = pp.verbose;
