a2d681b3fadeec17a3ad9d6e6bd533032704e64b
majid <alinejad.mj@gmail.com>
2023-08-08 09:17:44 +0330

    update .npmrc, package.json and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
 import { Consoli } from "../src/consoli";
 
-// export const consoliInstance = new consoli({
-//   nodeEnv: import.meta.env.MODE,
-//   password: import.meta.env.VITE_CONSOLI,
-//   defaultDeveloperMode: ["SUCCESS"],
-//   onMessageCallback: () => {},
-//   tags: [{ displayName: "myTag", color: "#9f00a1" }],
-// });
+export const consoliInstance = new Consoli({
+  //@ts-ignore
+  nodeEnv: import.meta.env.MODE,
+  //@ts-ignore
+  password: import.meta.env.VITE_CONSOLI,
+  defaultDeveloperMode: ["SUCCESS"],
+  onMessageCallback: () => {},
+  tags: [{ displayName: "myTag", color: "#9f00a1" }],
+});
 
-// consoliInstance.log("it a log consoli 💎💎");
-// consoliInstance.warn("it a warn consoli 🔶🔶");
-// consoliInstance.error("it a error consoli 🧯🧯");
-// consoliInstance.success("it a success consoli 🔋🔋");
-// consoliInstance.tags.myTag("it a my tag consoli 🍇🍇");
-// //@ts-ignore
-// window.verbose = consoliInstance;
-// const colasd = new Consoli({
-//   nodeEnv: import.meta.env.MODE,
-//   password: import.meta.env.VITE_CONSOLI,
-//   defaultDeveloperMode: ["SUCCESS"],
-//   onMessageCallback: () => {},
-//   tags: [{ displayName: "myTag", color: "#9f00a1" }],
-// });
+consoliInstance.log("it a log consoli 💎💎");
+consoliInstance.warn("it a warn consoli 🔶🔶");
+consoliInstance.error("it a error consoli 🧯🧯");
+consoliInstance.success("it a success consoli 🔋🔋");
+consoliInstance.tags?.myTag("it a my tag consoli 🍇🍇");
 
-// console.log(colasd);
+//@ts-ignore
+window.verbose = consoliInstance;
