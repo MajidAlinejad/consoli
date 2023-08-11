30ed0c495931b4e4eac82fd200ff6e00ba68e62b
majid <alinejad.mj@gmail.com>
2023-08-11 13:39:26 +0330

    update 11 files and delete 4 files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    // vite config
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
